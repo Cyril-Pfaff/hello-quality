533c648ac3f015ac583d964d204d6e33f240213e
Cyril Pfaff <cp512@cylian.org>
2021-10-07 16:43:11 +0200

    Corection

--- src/main/java/org/cylian/hello/App.java
+++ src/main/java/org/cylian/hello/App.java
@@ -4,32 +4,27 @@ package org.cylian.hello;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 
-	private int secure;
-	
 	/**
 	 * Une variable
 	 */
-	public static String ip = "127.0.0.1";
-	
+	public static String ip = "localhost";
+
 	/**
 	 * Le point d'entrée
 	 * @param args Les arguments
 	 */
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ){
         System.out.println( "Hello World!" );
-		
+
 		String[] fields = { "a","z","e","r","t","y"};
 		String out = "";
-		
-		for(int i=0; i<fields.length; i++)
-		{
+
+		for(int i=0; i<fields.length; i++){
 			out = out + fields[i];
 		}
-		
-		System.out.println( "Field said: " + out + " from "+ip);
+
+			System.out.println( "Field said: " + out + " from "+ip);
     }
 }
