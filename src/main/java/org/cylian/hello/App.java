94779985a4698e1861e4a497472885c5aaf82294
Cyril Pfaff <cp512@cylian.org>
2021-10-07 13:25:32 +0200

    Initial commit

---
+++ src/main/java/org/cylian/hello/App.java
@@ -0,0 +1,35 @@
+package org.cylian.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	private int secure;
+	
+	/**
+	 * Une variable
+	 */
+	public static String ip = "127.0.0.1";
+	
+	/**
+	 * Le point d'entrée
+	 * @param args Les arguments
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		
+		String[] fields = { "a","z","e","r","t","y"};
+		String out = "";
+		
+		for(int i=0; i<fields.length; i++)
+		{
+			out = out + fields[i];
+		}
+		
+		System.out.println( "Field said: " + out + " from "+ip);
+    }
+}
